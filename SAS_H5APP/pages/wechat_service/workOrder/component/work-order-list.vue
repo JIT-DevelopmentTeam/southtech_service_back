e270978d380b7803256bec2ecd0986d79d6bce88
Vigny <944319975@qq.com>
2020-04-03 15:01:30 +0800

    Vigny

---
+++ SAS_H5APP/pages/wechat_service/workOrder/component/work-order-list.vue
@@ -0,0 +1,155 @@
+<template>
+	<view @click="goDetail(info.id)">
+		<uni-swipe-action-item :options="options" @click="onClick" @change="change(info.id)" >
+			<uni-card class="uniCard" note="true">
+				<view class="info">
+					<view class="line">
+						<view class="label">{{ info.clientName }}</view>
+					</view>
+					<view class="line">
+						<span class="iconfont iconhuizhengongzuoliang iconStyle"></span>
+						<view class="label sameLine">联系人：{{ info.contactName }}</view>
+						<view class="label sameLine">{{ info.contactPhone }}</view>
+					</view>
+					<view class="line">
+						<location :labelStyle="label" :label="info.province+info.city+info.area+info.community+info.address" :left_right="left_right"></location>
+					</view>
+				</view>
+				<view class="btn">
+					<model-label :modelLabel="formatModel"></model-label>
+				</view>
+				<template v-slot:footer>
+					<view class="footer-box">
+						<view class="label">{{ formatDate(info.assignedTime) }}</view>
+						<view class="label right">{{ calTime(formatDate(info.assignedTime)) }}</view>
+					</view>
+				</template>
+			</uni-card>
+		</uni-swipe-action-item>
+	</view>
+</template>
+
+<script>
+	import {calculationTime} from '@/utils/moment.js'
+	import {format} from '@/utils/formatDate.js'
+
+	export default {
+		name: 'workOrderList',
+		data() {
+			return {
+				workOrderId:'',
+				label: {
+					'fontSize': '30upx',
+					'display': 'inline-block'
+				},
+				left_right: '左',
+				options:[
+					{
+						text: '服务评价',
+						style: {
+							backgroundColor: '#FFA500'
+						},
+						url:"../evaluate/evaluate"
+					}
+				],
+			}
+		},
+		components: {
+			uniCard: () => import('@dcloudio/uni-ui/lib/uni-card/uni-card.vue'), 
+			location: () => import('@/components/location/location.vue'), 
+			phone: () => import('@/components/phone/phone.vue'), 
+			modelLabel: () => import('@/components/model-label/model-label.vue'),
+			uniSwipeActionItem: () => import('@dcloudio/uni-ui/lib/uni-swipe-action-item/uni-swipe-action-item.vue')
+		},
+		props: {
+			'info': Object
+		},
+		computed: {
+			formatDate(dateTime) {
+				return dateTime => {
+					return format(dateTime)
+				}
+			},
+			calTime(dateTime) {
+				return dateTime => {
+					return calculationTime(dateTime)
+				}
+			},
+			formatModel() {
+				let dic = this.$store.getters['dic/getServiceTypeList']
+				let result
+				if (dic != '') {
+					result = dic.filter(e=>e.value == this.info.type)[0].text
+				} else {
+					result = ''
+				}
+				return result
+			}
+		},
+		methods: {
+			goDetail(id) {
+				uni.setStorageSync('ticketId', id)
+				uni.navigateTo({
+					url: './detail/work-order-detail'
+				})
+			},
+			onClick(e){
+				if (e.index == 0) {
+					uni.navigateTo({
+						url: e.content.url +'?workOrderId=' + this.workOrderId
+					})
+				}
+			},
+			change(id){
+				this.workOrderId = id;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.uniCard {
+		margin: 10upx !important;
+	}
+
+	.label {
+		font-size: 30upx;
+		display: inline-block;
+	}
+
+	.sameLine {
+		display: inline;
+		margin-right: 35upx;
+	}
+
+	.iconStyle {
+		margin-right: 25upx;
+	}
+
+	.line {
+		margin-bottom: 15upx;
+	}
+
+	.info {
+		width: fit-content;
+		display: inline-block;
+	}
+
+	.btn {
+		display: inline-block;
+	}
+
+	.phone {
+		top: 120upx;
+	}
+
+	.right {
+		position: absolute;
+		right: 10upx;
+		border: 1rpx solid #ffe289;
+		color: #fec6c6;
+		padding: 10upx;
+		margin-top: -7upx;
+	}
+	
+</style>
---
+++ SAS_H5APP/pages/wechat_service/workOrder/index.vue
@@ -0,0 +1,133 @@
+<template>
+	<view class="content">
+		<uni-segmented-control class="borderStyle" :current="current" :values="items" style-type="button" active-color="#09a0f7" @clickItem="onClickItem"></uni-segmented-control>
+		<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltoupper="upper" @scrolltolower="lower" @scroll="scroll">
+			<uni-swipe-action id="swipe">
+				<work-order-list v-for="(item,index) in dataSource" :info="item" :key="index"></work-order-list>
+			</uni-swipe-action>
+			<uni-load-more :status="'loading'" v-show="load"></uni-load-more>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'subSection',
+		components: {
+			uniSegmentedControl: () => import('@dcloudio/uni-ui/lib/uni-segmented-control/uni-segmented-control.vue'), 
+			workOrderList: () => import('./component/work-order-list.vue'),
+			uniLoadMore: () => import('@dcloudio/uni-ui/lib/uni-load-more/uni-load-more.vue'),
+			uniSwipeAction: () => import('@dcloudio/uni-ui/lib/uni-swipe-action/uni-swipe-action.vue')
+		},
+		data() {
+			return {
+				items: ['未完工','已完工','全部'],
+				current: 0,
+				scrollTop: 0,
+				old: {
+					scrollTop: 0
+				},
+				load: false,
+				pageNo: 1,
+				pageSize: 5,
+				dataSource: [],
+				total: 0
+			}
+		},
+		methods: {
+			/**
+			 * 点击segmentedControl 事件回调
+			 */
+			onClickItem(e) {
+				if (this.current !== e.currentIndex) {
+					this.current = e.currentIndex;
+					this.dataSource = [];
+					this.pageNo = 1;
+					let obj = {
+						openId: this.$store.getters['getWeChatUser'],
+						status: this.current,
+						pageNo: this.pageNo,
+						pageSize: this.pageSize
+					}
+					this.loadTicketList(obj)
+				}
+			},
+			upper() {
+				// console.log("到顶了");
+			},
+			lower() {
+				// console.log("到底了");
+				if (this.dataSource.length != this.total) {
+					this.load = true;
+					this.pageNo += 1;
+					let obj = {
+						openId: this.$store.getters['getWeChatUser'],
+						status: this.current,
+						pageNo: this.pageNo,
+						pageSize: this.pageSize
+					}
+					this.loadTicketList(obj)
+				}
+			},
+			scroll() {
+				// console.log("滚动了");
+			},
+			loadTicketList(obj) {
+				this.$store.dispatch('workOrder/GetServiceDataList', obj).then(res=>{
+					this.total = res.total
+					let result = res.list
+					result.forEach(item => {
+						this.dataSource.push(item)
+						this.load = false;
+					})
+					const dataList = this.dataSource.slice(0);
+					this.$store.commit('workOrder/setServiceTicketList', dataList)
+				});
+			}
+		},
+		onLoad(option) {
+			this.$store.dispatch('dic/GetServiceTypeList', 'work_order_type').then(res=>{
+				let obj = {
+					openId: this.$store.getters['getWeChatUser'],
+					status: this.current,
+					pageNo: this.pageNo,
+					pageSize: this.pageSize
+				}
+				this.loadTicketList(obj)
+			})
+		}
+	}
+</script>
+
+<style scoped>
+	.borderStyle {
+		margin: 15upx;
+	}
+	.borderStyle /deep/ .segmented-control__text {
+		font-size: 0.6rem !important;
+	}
+	/* iphone X */
+	@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+		.scroll-Y {
+			height: calc(100vh - 180px - 36px - 100px);
+		}
+	}
+	/* iphone 6~8 */
+	@media only screen and (device-width : 375px) and (device-height : 667px) and (-webkit-device-pixel-ratio : 2) {
+		.scroll-Y {
+			height: calc(100vh - 180px - 36px - 70px);
+		}
+	}
+	/* iphone 6 plus~8 plus */
+	@media only screen and (device-width : 414px) and (device-height : 736px) and (-webkit-device-pixel-ratio : 3) {
+		.scroll-Y {
+			height: calc(100vh - 180px - 36px - 70px);
+		}
+	}
+	.scroll-Y {
+		height: calc(100vh - 29px - 60px);
+	}
+	#swipe /deep/ .uni-swipe_box {
+		background-color: transparent !important;
+	}
+</style>
